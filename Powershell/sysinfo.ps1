6fcd0c82be558ac31d666fc734093a6a6f813dda
Ethan McMellan <mcmellem@mail.uc.edu>
2020-02-04 18:31:26 -0500

    adding powershell files

---
+++ Powershell/sysinfo.ps1
@@ -0,0 +1,10 @@
+﻿$Hello = "Hello, Powershell"
+function getIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+$IP = getIP
+
+Write-Host($Hello)
+
+
+write-host("The IP address of this machine is $IP")
