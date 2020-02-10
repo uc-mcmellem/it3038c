d00bfdb40c2412b0b83e0420ce5d4c78d1e2522b
uc-mcmellem <59898798+uc-mcmellem@users.noreply.github.com>
2020-02-09 22:09:55 -0500

    Create Project1_PowershellScript.ps1
    Project 1 PowerShell script to find all of the files in C:\Users\ that are bigger than 200 MB and list them

---
+++ Powershell/Project1_PowershellScript.ps1
@@ -0,0 +1,6 @@
+﻿##The first line gets all of the files in the path specified (C:\Users\ in this case
+Get-ChildItem -Path C:\Users\ -Recurse |
+##The second line says only get the files that are greater than 200 MB
+Where-Object {$_.length/1MB -gt 200} | 
+##Then select the file name, size in MB, an full path and display them in a list
+select name,@{n=”Size in MB”;e={$_.length/1MB}},fullname 
