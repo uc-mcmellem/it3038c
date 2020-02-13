04cba8f664a5fd04bbf4715bacff8612e8a5bb6f
uc-mcmellem <59898798+uc-mcmellem@users.noreply.github.com>
2020-02-13 08:27:07 -0500

    ignore

--- Project 1/Project1_PowershellScript.ps1
+++ Project 1/Project1_PowershellScript.ps1
@@ -3,4 +3,5 @@ Get-ChildItem -Path C:\Users\ -Recurse |
 ##The second line says only get the files that are greater than 200 MB
 Where-Object {$_.length/1MB -gt 200} | 
 ##Then select the file name, size in MB, an full path and display them in a list
-select name,@{n=”Size in MB”;e={$_.length/1MB}},fullname 
+select name,@{n=”Size in MB”;e={$_.length/1MB}},fullname | Sort-Object -Property "Size in MB" | 
+Out-File -FilePath 'C:\Users\'
